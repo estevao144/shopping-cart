908e172d17ad75c1d9279f96ebe27759aa45c596
João Henrique <joaohenriqueblocked@gmail.com>
2022-09-08 15:36:07 -0300

    Começando projeto vamos que vamos!

---
+++ src/pages/ListProduct.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListProducts extends Component {
+  render() {
+    return (
+      <div>
+        Listagem de produtos... requisito 2
+      </div>
+    );
+  }
+}
+
+export default ListProducts;
